a93c59edd74c2e15a9b9857453da34b834a857bf
Sara VT <111016536+smavt93@users.noreply.github.com>
2022-12-13 17:50:37 -0500

    Update attempt 3

--- Interactive_Dashboard.py
+++ Interactive_Dashboard.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from itertools import islice
-import requests
 pd.options.mode.chained_assignment = None
 
 st.title('VUMC Diagnoses')
